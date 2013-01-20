007e591d91b3d45ac9246759cb765ae6ab4fac1b
uturuncoglu <u.utku.turuncoglu@be.itu.edu.tr>
2013-01-20 15:08:51 +0100

    (regesm) : fix for esm component. start to run in atm

---
+++ mod_esmf_rtm.F90
@@ -0,0 +1,60 @@
+!-----------------------------------------------------------------------
+!
+!     This file is part of ICTP RegESM.
+!
+!     ICTP RegESM is free software: you can redistribute it and/or modify
+!     it under the terms of the GNU General Public License as published by
+!     the Free Software Foundation, either version 3 of the License, or
+!     (at your option) any later version.
+!
+!     ICTP RegESM is distributed in the hope that it will be useful,
+!     but WITHOUT ANY WARRANTY; without even the implied warranty of
+!     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!     GNU General Public License for more details.
+!
+!     You should have received a copy of the GNU General Public License
+!     along with ICTP RegESM.  If not, see <http://www.gnu.org/licenses/>.
+!
+!-----------------------------------------------------------------------
+#define FILENAME "mod_esmf_rtm.F90"
+!
+!-----------------------------------------------------------------------
+!     RTM gridded component code 
+!-----------------------------------------------------------------------
+!
+      module mod_esmf_rtm
+!
+!-----------------------------------------------------------------------
+!     Used module declarations 
+!-----------------------------------------------------------------------
+!
+      use ESMF
+!
+      use mod_types
+!
+      implicit none
+      private
+!
+!-----------------------------------------------------------------------
+!     Public subroutines 
+!-----------------------------------------------------------------------
+!
+      public :: RTM_SetServices
+!
+      contains
+!
+      subroutine RTM_SetServices(gcomp, rc)
+      implicit none
+!
+!-----------------------------------------------------------------------
+!     Imported variable declarations 
+!-----------------------------------------------------------------------
+!
+      type(ESMF_GridComp) :: gcomp
+      integer, intent(inout) :: rc
+!
+      rc = ESMF_SUCCESS
+!
+      end subroutine RTM_SetServices
+!
+      end module mod_esmf_rtm
