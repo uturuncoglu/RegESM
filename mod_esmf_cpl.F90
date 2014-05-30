7c23842a2c2efaa8707d53d76875397483e5e050
Ufuk Turunçoğlu <u.utku.turuncoglu@be.itu.edu.tr>
2014-05-30 10:34:09 +0300

    Fix for integral calculation

--- mod_esmf_cpl.F90
+++ mod_esmf_cpl.F90
@@ -358,7 +358,6 @@
       call ESMF_FieldRegridStore(srcField=srcField,                     &
                                  dstField=dstField,                     &
                                  srcMaskValues=(/models(iSrc)%isLand/), &
-                                 dstMaskValues=(/models(iDst)%isLand/), &
                                  srcFracField=srcFrac,                  &
                                  dstFracField=dstFrac,                  &
                                  unmappedaction=unmap,                  &
