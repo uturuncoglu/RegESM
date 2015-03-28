526ffbb17b443b43265426320901081075d5ea88
Ufuk Turunçoğlu <u.utku.turuncoglu@be.itu.edu.tr>
2015-03-28 11:28:07 +0200

    fix SetFinalize routine

--- mod_esmf_atm.F90
+++ mod_esmf_atm.F90
@@ -1275,22 +1275,6 @@
 !
       call ATM_Finalize()
 !
-!-----------------------------------------------------------------------
-!     Destroy ESMF objects 
-!-----------------------------------------------------------------------
-!
-      call ESMF_ClockDestroy(clock, rc=rc)
-      if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU,    &
-                             line=__LINE__, file=FILENAME)) return
-!
-      call ESMF_StateDestroy(importState, rc=rc)
-      if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU,    &
-                             line=__LINE__, file=FILENAME)) return
-!
-      call ESMF_StateDestroy(exportState, rc=rc)
-      if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU,    &
-                             line=__LINE__, file=FILENAME)) return
-!
       end subroutine ATM_SetFinalize
 !
       subroutine ATM_Get(gcomp, rc)
