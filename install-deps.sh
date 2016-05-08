2c656105abf189b874dabc70957a66b04a4cf971
uturuncoglu <u.utku.turuncoglu@be.itu.edu.tr>
2016-05-08 20:24:48 +0200

    (regesm) : minor fix in log redirection

--- install-deps.sh
+++ install-deps.sh
@@ -17,8 +17,8 @@ wget "http://zlib.net/zlib-${ZLIB_VER}.tar.gz"
 tar -zxvf zlib-${ZLIB_VER}.tar.gz
 cd zlib-${ZLIB_VER}
 ./configure --prefix=${PROGS}/zlib-${ZLIB_VER}
-make >& make.log 
-make install >>& make.log
+make > make.log 
+make install >> make.log
 export LD_LIBRARY_PATH=${PROGS}/zlib-${ZLIB_VER}/lib:${LD_LIBRARY_PATH}
 echo "LD_LIBRARY_PATH=${PROGS}/zlib-${ZLIB_VER}/lib:${LD_LIBRARY_PATH}" >> ~/.bashrc
 
@@ -28,7 +28,7 @@ wget "https://www.hdfgroup.org/ftp/HDF5/releases/hdf5-${HDF5_VER}/src/hdf5-${HDF
 tar -zxvf hdf5-${HDF5_VER}.tar.gz
 cd hdf5-${HDF5_VER}
 ./configure --prefix=$PROGS/hdf5-${HDF5_VER} --with-zlib=$PROGS/zlib-${ZLIB_VER} --enable-fortran --enable-cxx CC=${CC} FC=${FC} CXX=${CXX}
-make >& make.log
-make install >>& make.log
+make > make.log
+make install >> make.log
 export LD_LIBRARY_PATH=$PROGS/hdf5-${HDF5_VER}/lib:$LD_LIBRARY_PATH
 echo "export LD_LIBRARY_PATH=$PROGS/hdf5-${HDF5_VER}/lib:$LD_LIBRARY_PATH" >> ~/.bashrc
