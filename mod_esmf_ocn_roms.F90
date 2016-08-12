3a351ccef8141ef2324ec5959cd1e169e296a715
uturuncoglu <u.utku.turuncoglu@be.itu.edu.tr>
2016-08-12 22:43:44 +0300

    (regesm) : minor fix for issue #15

--- mod_esmf_ocn_roms.F90
+++ mod_esmf_ocn_roms.F90
@@ -849,11 +849,11 @@
 !
       if (models(Iocean)%mesh(i)%gtype == Iupoint) then
         staggerLoc = ESMF_STAGGERLOC_EDGE1
-        staggerEdgeLWidth = (/0,1/)
-        staggerEdgeUWidth = (/0,0/)
+        staggerEdgeLWidth = (/0,0/)
+        staggerEdgeUWidth = (/0,1/)
       else if (models(Iocean)%mesh(i)%gtype == Ivpoint) then
         staggerLoc = ESMF_STAGGERLOC_EDGE2
-        staggerEdgeLWidth = (/0,0/)
+        staggerEdgeLWidth = (/1,0/)
         staggerEdgeUWidth = (/0,1/)
       else if (models(Iocean)%mesh(i)%gtype == Icross) then
         staggerLoc = ESMF_STAGGERLOC_CENTER
@@ -862,7 +862,7 @@
       else if (models(Iocean)%mesh(i)%gtype == Idot) then
         staggerLoc = ESMF_STAGGERLOC_CORNER
         staggerEdgeLWidth = (/0,0/)
-        staggerEdgeUWidth = (/0,0/)
+        staggerEdgeUWidth = (/0,1/)
       end if
 !
 !-----------------------------------------------------------------------
